d07fce7f6f1a53d474fe767d64c259fa6f7f52ce
Palak Patel <palakpatel910@gmail.com>
2024-06-06 19:44:00 -0400

    Updated module run_simulation

--- run_simulation.py
+++ run_simulation.py
@@ -26,4 +26,19 @@ def make_sim_dir(template_dir : str, sim_dir : str = None, overwrite=True):
     shutil.copytree(template_dir, sim_dir)
     print(f"Created Simulation Dir {sim_dir}")
 
+def update_initial_boundary_values(field, value, inlet_value = None):
+    """
+    Update the value in the 'field' file inside the simulation directory (0/) for initial and boundary condition.
+    """
+    field_file = os.path.join(SIM_DIR, "0", field)
+
+    with open(field_file) as f:
+        template = Template(f.read())
+
+    with open(field_file, "w") as f:
+        if field == "U":
+            f.write(template.safe_substitute(internal_field_value = value, inlet_value = inlet_value))
+        else:
+            f.write(template.safe_substitute(internal_field_value = value))
+        print(f"Updated {field} Value")
 
